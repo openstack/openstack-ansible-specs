2b72a22f778aa9c434a5571a55815300c4260e65
sonpn13 <sonpn13@viettel.com.vn>
2019-10-27 08:59:23 +0700

    PDF Documentation Build tox target
    Change-Id: I54e3d3d6fbe0b753597703b6e61f884703c4e7e7
    Signed-off-by: sonpn13 <sonpn13@viettel.com.vn>
    Story: 2006105
    Task: 35341

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,6 +37,8 @@
 # ones.
 extensions = [
     'openstackdocstheme',
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.rsvgconverter',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -61,11 +63,11 @@ description = 'OpenStack-Ansible deploys OpenStack environments using Ansible.'
 project = 'OpenStack-Ansible'
 role_name = 'specs'
 target_name = 'openstack-ansible-' + role_name
-title = 'OpenStack-Ansible ' + role_name + ' Documentation'
-
+title = 'OpenStack-Ansible Documentation: ' + role_name + ' role'
 
 # openstackdocstheme options
 openstackdocs_repo_name = 'openstack/' + target_name
+openstackdocs_pdf_link = True
 openstackdocs_bug_project = project.lower()
 openstackdocs_bug_tag = ''
 
@@ -228,9 +230,11 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, target_name + '.tex',
-     title, author, 'manual'),
+    (master_doc, 'doc-' + target_name + '.tex',
+     title.replace("_", r"\_"), author, 'manual'),
 ]
+
+latex_use_xindy = False
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -288,3 +292,11 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
+# -- Options for PDF output --------------------------------------------------
+
+pdf_documents = [
+    (master_doc, target_name,
+     title, author)
+]
+
+locale_dirs = ['locale/']
